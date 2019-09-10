115430d247325ea4db5061ee826f80668ef89521
santoshSrinivasa <santoshsrinivasa94@gmail.com>
2019-09-10 18:15:36 +0530

    3 files modified

--- UserLogin.js
+++ UserLogin.js
@@ -13,13 +13,13 @@ export default class UserLogin extends React.Component
       <h3 className="welcomeHeader">Welcome to Xyz project</h3>
       <div className="col-md-12">
       <label>User name : </label>
-      <input type="text" name="username" onChange={this.props.onChange}/>
+      <input type="text" name="userNameEntered" onChange={this.props.onChange}/>
       </div>
       <div className="col-md-12">
       <label>Password : </label>
-      <input type="password" name="password" onChange={this.props.onChange}/>
+      <input type="password" name="passwordEntered" onChange={this.props.onChange}/>
       </div>
-      <button type="button">Login</button>
+      <button type="button" onClick={this.props.onClick}>Login</button>
       </div>
     );
   }
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import './style.css';
 import UserLogin from "./UserLogin";
+import Welcome from "./Welcome";
+import BrowserRouter from "react-router";
 
 class App extends Component {
   constructor() {
@@ -9,23 +11,40 @@ class App extends Component {
     this.state = {
       username:"Santosh",
       password:12345,
-      userNameEntered: '',
-      passwordEntered: '',
+      userNameEntered: "",
+      passwordEntered: "",
       isloginValid:false,
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   handleChange(event)
   {
     const name = event.target.name;
     const value = event.target.type === "text" ? event.target.value : event.target.value;
-    console.log(name);
-    console.log(value);
+    if(name == "userNameEntered")
+    this.setState({userNameEntered: value});
+    if(name == "passwordEntered")
+    this.setState({passwordEntered: value}); 
   }
-  
+  handleClick(event)
+  {
+    event.preventDefault();
+    const nameDefault = this.state.username;
+    const pwdDefault = this.state.password;
+    const nameEntered = this.state.userNameEntered;
+    const pwdEntered = this.state.passwordEntered
+    if((nameDefault == nameEntered)&&(pwdDefault == pwdEntered)){
+      this.setState({isloginValid: true});
+      console.log(this.state.isloginValid)
+    }else{
+      console.log(this.state.isloginValid)
+    }
+  }
   render() {
     return (
       <div>
-        <UserLogin onChange={this.handleChange}/>
+        <UserLogin onChange={this.handleChange} onClick={this.handleClick}/>
       </div>
     );
   }
